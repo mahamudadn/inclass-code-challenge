65b620d5e3efcc391daab28ef0aece6dbb321240
Mahamud Aden <mahamudadn@gmail.com>
2023-05-15 14:14:17 -0500

    added a getjokes that gets jokes from the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,16 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+//click listeners
+    $('#add-Joke').on('click', submitJoke)
    
+}
+
+// Function That gets jokes from the server
+
+function getJokes(event) {
+    // $.ajax({
+
+    // })
+  
 }
